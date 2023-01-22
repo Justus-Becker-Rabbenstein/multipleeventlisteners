b6a540687d6fa8307bf3f773ff90b4f6934f1ad8
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-01-22 12:03:59 +0100

    put some colors for easier comprehension

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,46 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function Card({char}){
+
     const [detailsState, setDetailsState] = useState(false);
+
     function toggleDetailsState(){
       setDetailsState(!detailsState)
     }
+
     return <div>
-    <p>{char.name}</p>
-    {detailsState && (
-      <>
-        <p>{char.type}</p>
-        <p>{char.gender}</p>
-      </>
-    )}
+    <StyledFlexDiv>
+    <StyledP>Always visible</StyledP>
+    &nbsp;
+    <StyledP>{char.name}</StyledP>
+    </StyledFlexDiv>
     <button onClick={()=>toggleDetailsState()}>ShowInfo</button>
+    {detailsState ? 
+    (
+      <StyledDiv>
+        <StyledFlexDiv>
+        <p>Conditional rendered</p>
+        <p>{char.type}</p>
+        &nbsp;
+        <p>{char.gender}</p>
+        </StyledFlexDiv>
+      </StyledDiv>
+    )
+    :
+    ""
+  }
   </div>
 }
+
+const StyledP = styled.p`
+border: 1px solid black;
+background-color: green;
+`
+const StyledDiv = styled.div`
+border: 1px solid black;
+background-color: crimson;
+`
+const StyledFlexDiv = styled.div`
+display: flex;
+`
